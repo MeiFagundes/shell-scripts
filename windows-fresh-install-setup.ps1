71dff9ce0b9846f89754e66fea4b176edebf7124
Mei Fagundes <mei@meifagundes.com>
2021-11-15 08:33:55 -0300

    Add install options for multiple use cases and other improvements

--- windows-fresh-install-setup.ps1
+++ windows-fresh-install-setup.ps1
@@ -1,70 +1,118 @@
 #Requires -RunAsAdministrator
 
-# install scoop command-line installer
-iwr -useb get.scoop.sh | iex;
+Write-Host "`n-----------------------------------------------------------------------------------------------" -ForegroundColor cyan
+Write-Host "--------------------- Initializing Mei Windows Fresh Install Setup Script ---------------------" -ForegroundColor cyan
+Write-Host "-----------------------------------------------------------------------------------------------`n" -ForegroundColor cyan
 
-# Add required buckets
-scoop bucket add games;
-scoop bucket add extras;
-scoop bucket add java;
+$shouldInstallDefaultApps = (Read-Host "Do you want to install the default apps? [y/N]") -eq 'y'
+$shouldInstallDevelopmentApps = (Read-Host "Do you want to setup this machine for development? [y/N]") -eq 'y'
+$shouldInstallGamingApps = (Read-Host "Do you want to setup this machine for gaming? [y/N]") -eq 'y'
+$shouldInstallWorkApps = (Read-Host "Do you want to setup this machine for work? [y/N]") -eq 'y'
+$shouldInstallFonts = (Read-Host "Do you want to install fonts? [y/N]") -eq 'y'
+$shouldInstallAdminTools = (Read-Host "Do you want to install system admin tools? [y/N]") -eq 'y'
 
-scoop update;
-scoop install aria2; # Scoop uses aria2c for multi-connection downloads
+# Installing Git and reloading system and user environment variables
+winget install --id=Git.Git -e -h
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 
-# Default App installs
-scoop install 7zip;
-regedit /s $ENV:UserProfile\scoop\apps\7zip\current\install-context.reg;
-scoop install deluge;
-scoop install rufus;
-scoop install teamviewer;
-scoop install gimp;
-scoop install discord
-winget install --id=Microsoft.Teams -e -h --force; 
-winget install --id=da2x.EdgeDeflector -e -h --force; 
-winget install --id=MiniTool.PartitionWizard.Free -e -h --force; 
-winget install --id=Parsec.Parsec -e -h --force; 
-winget install --id=Stremio.Stremio -e -h --force; 
+# Installing scoop command-line installer
+iwr -useb get.scoop.sh | iex
 
-# Gaming Apps
-scoop install retroarch;
-scoop install pcsx2-dev;
-scoop install dolphin-beta;
-scoop install cpu-z;
-scoop install gpu-z;
-scoop install hwmonitor;
-scoop install vortex;
-winget install --id=Valve.Steam -e -h --force; 
-winget install --id=EpicGames.EpicGamesLauncher -e -h --force; 
-winget install --id=Ubisoft.Connect -e -h --force;
+# Adding required buckets and updating scoop
+scoop bucket add games
+scoop bucket add extras
+scoop bucket add java
+scoop bucket add nerd-fonts
+scoop update
 
-# Development Apps
-scoop install git;
-scoop install gitkraken
-scoop install nodejs;
-scoop install yarn;
-scoop install openjdk;
-scoop install openjdk8-redhat-jre;
-scoop install android-studio;
-scoop install vscode;
-# Add Visual Studio Code as a context menu option:
-regedit /s $ENV:UserProfile\scoop\apps\vscode\current\install-context.reg;
+# Default Installs
+if ($shouldInstallDefaultApps) {
+    Write-Host "`n--------------------------------- Installing Default Apps ---------------------------------`n" -ForegroundColor cyan
+    scoop install 7zip
+    regedit /s $home\scoop\apps\7zip\current\install-context.reg # Adding 7-Zip as a context menu option
+    scoop install teamviewer
+    scoop install discord
+    scoop install deluge
+    scoop install gimp
+    winget install --id=Parsec.Parsec -e -h
+    winget install --id=Stremio.Stremio -e -h
+    Write-Output "`n- Installing all Visual C++ Redistributable runtime libraries`n"
+    scoop install vcredist
+    scoop uninstall vcredist2005 vcredist2008 vcredist2010 vcredist2012 vcredist2013 vcredist # Cleaning installers after installing VC++ Redistributable on the system
+    Write-Host "`n- All Visual C++ Redistributable runtime libraries were installed successfully" -ForegroundColor green
+}
 
-# ECHO Y | scoop install android-sdk;
-# # reload system and user environment variables
-# $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User");
-# sdkmanager --install "cmdline-tools;latest";
-# cmd.exe /c  "$ENV:UserProfile\scoop\apps\android-sdk\current\tools\bin\sdkmanager.bat";
+# Development
+if ($shouldInstallDevelopmentApps) {
+    Write-Host "`n------------------------------- Installing Development Tools ------------------------------`n" -ForegroundColor cyan
+    scoop install gitkraken
+    scoop install nodejs
+    scoop install yarn
+    scoop install openjdk
+    scoop install openjdk8-redhat-jre
+    scoop install android-studio
+    scoop install vscode
+    regedit /s $home\scoop\apps\vscode\current\install-context.reg # Adding Visual Studio Code as a context menu option
+    winget install --id=UnityTechnologies.UnityHub -e -h
+}
 
-# ECHO Y | scoop install flutter;
-# # accept all SDK package licenses
-# , 'y' * 10  | flutter doctor --android-licenses;
+# Gaming
+if ($shouldInstallGamingApps) {
+    Write-Host "`n------------------------- Installing Game Launchers and Emulators -------------------------`n" -ForegroundColor cyan
+    scoop install retroarch
+    scoop install pcsx2-dev
+    scoop install dolphin-beta
+    scoop install cemu
+    scoop install cemuhook
+    winget install --id=Valve.Steam -e -h
+    winget install --id=EpicGames.EpicGamesLauncher -e -h
+    winget install --id=Ubisoft.Connect -e -h --force
+    winget install --id=NexusMods.Vortex -e -h
+}
 
-winget install --id=UnityTechnologies.UnityHub -e -h --force; 
+# Work
+if ($shouldInstallWorkApps) {
+    Write-Host "`n----------------------------- Installing Work Apps and Tools ------------------------------`n" -ForegroundColor cyan
+    winget install --id=Microsoft.Teams -e -h
+    scoop install caffeine
 
-# Finalizing
+    # Configuring Caffeine to start at login and keep the screen awake during active hours (9 AM - 7 PM)
+    $WshShell = New-Object -comObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut("$home\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Caffeine.lnk")
+    $Shortcut.TargetPath = "$home\scoop\apps\caffeine\current\caffeine64.exe"
+    $Shortcut.Arguments = "-activehours:.........xxxxxxxxxx....."
+    $Shortcut.Save()
+    Write-Host "- Caffeine is now configured to automatically keep the screen awake during active hours (9 AM - 7 PM)" -ForegroundColor green
+}
 
-# Creates a Symbolic Link for the apps folder on C:
-New-Item -Path C:\Apps -ItemType SymbolicLink -Value $ENV:UserProfile\scoop\apps;
+# Fonts
+if ($shouldInstallFonts) {
+    Write-Host "`n------------------------------------- Installing Fonts ------------------------------------`n" -ForegroundColor cyan
+    scoop install Cascadia-Code
+    scoop install Open-Sans
+    scoop install Raleway
+    scoop install Ubuntu-NF
+    scoop install SourceCodePro-NF
+    scoop install SourceCodePro-NF-Mono
+}
 
-Write-Host -NoNewLine 'Press any key to continue...';
-$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+# System Admin Tools
+if ($shouldInstallAdminTools) {
+    Write-Host "`n------------------------------- Installing System Admin Tools -----------------------------`n" -ForegroundColor cyan
+    scoop install winaero-tweaker
+    scoop install rufus
+    scoop install cru
+    scoop install cpu-z
+    scoop install gpu-z
+    scoop install hwmonitor
+    winget install --id=MiniTool.PartitionWizard.Free -e -h
+}
+
+# Creating a Symbolic Link of the scoop apps folder on C: drive root
+New-Item -Path C:\Apps -ItemType SymbolicLink -Value $home\scoop\apps | Out-Null
+Write-Host "`n- A shortcut for the apps folder is now created at 'C:\Apps'" -ForegroundColor green
+Write-Output "nonportable apps were installed at the default location provided by each installer"
+
+Write-Host "`n-----------------------------------------------------------------------------------------------" -ForegroundColor cyan
+Write-Host "-------------------------------------- Setup Complete! ----------------------------------------" -ForegroundColor cyan
+Write-Host "-----------------------------------------------------------------------------------------------`n" -ForegroundColor cyan
